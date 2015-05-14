b0f8ad3053860cb38404d0b734d5298ffeccc68c
Philip Woelfel <is141524@fhstp.ac.at>
2015-05-14 14:48:18 +0200

    added rabbitmq auth example

---
+++ stuff/rabbitmq/auth.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import pika
+
+credentials = pika.PlainCredentials('philip', 'Passw0rd')
+parameters = pika.ConnectionParameters(credentials=credentials, host='localhost')
+connection = pika.BlockingConnection(parameters=parameters)
