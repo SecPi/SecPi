6505fa0e4e297bf77808df2a3839b7d6c5b5be92
Philip Woelfel <is141524@fhstp.ac.at>
2015-06-02 23:10:45 +0200

    implemented md5 check for config in worker implemented got_config right implemented alarm sending with json introduced active state variable added sensors cleanup

--- worker/worker.py
+++ worker/worker.py
@@ -7,6 +7,8 @@ import RPi.GPIO as GPIO
 import logging
 import importlib
 import threading
+import json
+import hashlib
 
 from tools import config
 from webcam import Webcam
@@ -14,11 +16,16 @@ from webcam import Webcam
 class Worker:
 
 	def __init__(self):
+		self.actors = []
+		self.active = True # start deactivated --> only for debug True
+		self.current_config_hash = None
+		
 		# setup gpio and logging
 		GPIO.setmode(GPIO.BCM)
 		logging.basicConfig(format='%(asctime)s | %(levelname)s:  %(message)s', level=logging.INFO)
 		
 		config.load("worker")
+		# TODO: generate md5 hash
 		
 		credentials = pika.PlainCredentials(config.get('rabbitmq')['user'], config.get('rabbitmq')['password'])
 		parameters = pika.ConnectionParameters(credentials=credentials, host=config.get('rabbitmq')['master_ip']) #this will change because we need the ip initially
@@ -26,58 +33,83 @@ class Worker:
 		self.channel = self.connection.channel()
 
 		#declare all the queues
-		self.channel.queue_declare(queue='%s_action' % config.get('pi_id'))
-		self.channel.queue_declare(queue='%s_config' % config.get('pi_id'))
+		self.channel.queue_declare(queue='%i_action' % config.get('pi_id'))
+		self.channel.queue_declare(queue='%i_config' % config.get('pi_id'))
 		self.channel.queue_declare(queue='data')
 		self.channel.queue_declare(queue='alarm')
 
 		#specify the queues we want to listen to, including the callback
-		self.channel.basic_consume(self.got_action,
-							  queue='%s_action' % config.get('pi_id'),
-							  no_ack=True)
+		self.channel.basic_consume(self.got_action, queue='%i_action' % config.get('pi_id'), no_ack=True)
 		
-		self.channel.basic_consume(self.got_config,
-							  queue='%s_config' % config.get('pi_id'),
-							  no_ack=True)
+		self.channel.basic_consume(self.got_config, queue='%i_config' % config.get('pi_id'), no_ack=True)
 
-		self.actors = []
+		
+		
+		self.channel.start_consuming()
 		self.setup_sensors()
 		self.setup_actors()
-		self.channel.start_consuming()
+		
+		
 		
 
 	def got_action(self, ch, method, properties, body):
-		# TODO: create/clear alarm_data folder
+		if(self.active):
+			# TODO: create/clear alarm_data folder
+			
+			# DONE: threading
+			#		http://stackoverflow.com/questions/15085348/what-is-the-use-of-join-in-python-threading
+			logging.info("received action from manager")
+			threads = []
+			
+			for act in self.actors:
+				t = threading.Thread(name='thread-%s'%(act.id), target=act.execute)
+				threads.append(t)
+				t.start()
+				# act.execute()
 		
-		# DONE: threading
-		#		http://stackoverflow.com/questions/15085348/what-is-the-use-of-join-in-python-threading
-		logging.info("received action from manager")
-		threads = []
+			# wait for threads to finish	
+			for t in threads:
+				t.join()
 		
-		for act in self.actors:
-			t = threading.Thread(name='thread-%s'%(act.id), target=act.execute)
-			threads.append(t)
-			t.start()
-			# act.execute()
-	
-		# wait for threads to finish	
-		for t in threads:
-			t.join()
-	
-		# TODO: get contents from alarm_data folder and send them over queue
-		# TODO: send finished
+			# TODO: get contents from alarm_data folder and send them over queue
+			# TODO: send finished
+		else:
+			logging.debug("received action but wasn't active")
 
 	def got_config(self, ch, method, properties, body):
-		print " [x] Received config %r" % (body,)
-		# TODO: check valid config file?!
-		# write config to file
-		f = open('config.json','w')
-		f.write(body)	
-		f.close()
-		# set new config
-		# config.load()
-		self.conf = config.getDict()
-		print "Config saved..."
+		logging.info("received config %r" % (body))
+		
+		# check hash of config to detect if new
+		m = hashlib.md5()
+		m.update(body)
+		# m.digest()
+		
+		# check if no hash exists (we don't have a config yet) or hash has changed (new config)
+		if(self.current_config_hash is None or self.current_config_hash != m.digest()):
+			# disable while loading config
+			self.active = false
+			
+			# TODO: deactivate queues
+			self.cleanup_sensors()
+			
+			# TODO: check valid config file?!
+			# write config to file
+			f = open('config.json','w') # TODO: pfad
+			f.write(body)
+			f.close()
+			
+			# set new config
+			config.load("worker")
+			
+			if(config.get('active')):
+				self.setup_sensors()
+				self.setup_actors()
+				# TODO: activate queues
+				self.active = True
+			
+			logging.info("Config saved...")
+		else:
+			logging.info("config the same")
 		
 	# Initialize all the sensors for operation and add callback method
 	def setup_sensors(self):
@@ -86,6 +118,10 @@ class Worker:
 			GPIO.add_event_detect(int(sensor["gpio"]), GPIO.RISING, callback=self.alarm, bouncetime=5000)		
 			logging.info("Registered sensor gpio: %s" % sensor["gpio"])
 	
+	def cleanup_sensors(self):
+		# remove the callbacks
+		for sensor in config.get("sensors"):
+			GPIO.remove_event_detect(int(sensor["gpio"]))
 	
 	# see: http://stackoverflow.com/questions/1176136/convert-string-to-python-class-object
 	def class_for_name(self, module_name, class_name):
@@ -96,30 +132,37 @@ class Worker:
 		c = getattr(m, class_name)
 		return c
 	
+	
 	# Initialize all the actors
 	def setup_actors(self):
-		# TODO replace self.conf 
 		for actor in config.get("actors"):
 			a = self.class_for_name(actor["module"], actor["class"])
 			act = a(actor["id"], actor["params"])
 			self.actors.append(act)
-			#if actor["type"] == "webcam":
-			#	self.actors.append(Webcam("/dev/video0", (640,480)))
+			logging.info("set up actor %s" % actor['class'])
 					
 
 	# callback for the sensors
 	def alarm(self, channel):
-		logging.info("Sensor at gpio %s detected something" % channel)
+		if(self.active):
+			logging.info("Sensor at gpio %s detected something" % channel)
 
-		# determine the id of the sensor which signaled
-		sensor_id = ""
-		for sensor in config.get("sensors"):
-			if int(sensor["gpio"]) == channel:
-				sensor_id = sensor["id"]	
-
-		# send a message to the alarmQ and tell which sensor signaled
-		self.channel.basic_publish(exchange='manager', routing_key='alarm',
-							body="%s" % sensor_id)
+			# determine the id of the sensor which signaled
+			sensor_id = ""
+			for sensor in config.get("sensors"):
+				if int(sensor["gpio"]) == channel:
+					sensor_id = sensor["id"]
+					break
+			
+			msg = {	"pi_id":config.get("pi_id"),
+					"sensor_id": sensor_id,
+					"gpio": channel}
+			
+			msg_string = json.dumps(msg)
+			
+			# send a message to the alarmQ and tell which sensor signaled
+			properties = pika.BasicProperties(content_type='application/json')
+			self.channel.basic_publish(exchange='manager', routing_key='alarm', body=msg_string, properties=properties)
 		
 		
 	def get_ip(self):
@@ -130,7 +173,10 @@ class Worker:
 		s.close()
 		
 		return ip
-
+	
+	def __del__(self):
+		self.connection.close()
+		GPIO.cleanup()
 
 
 if __name__ == '__main__':
