8f629839aa2d0005583f1202057af1df81cd3489
Martin Liebl <is141513@fhstp.ac.at>
2015-10-14 22:20:42 +0200

    tcpportlistener: removed garbage

--- worker/tcpportlistener.py
+++ worker/tcpportlistener.py
@@ -4,14 +4,13 @@ import SocketServer
 import threading
 from tools.sensor import Sensor
 
-
 class TCPPortListener(Sensor):
 
 	def __init__(self, id, params, worker):
 		super(TCPPortListener, self).__init__(id, params, worker)
 		self.active = False
-		address = (self.params["ip"], int(self.params["port"])
-		self.server = SecPiTCPServer(address, SecPiTCPHandler)
+		address = (self.params["ip"], int(self.params["port"]))
+		self.server = SocketServer.TCPServer(address, SecPiTCPHandler)
 
 	def activate(self):
 		self.active = True
@@ -26,57 +25,12 @@ class TCPPortListener(Sensor):
 
 # Request Handler
 import SocketServer
+
 class SecPiTCPHandler(SocketServer.BaseRequestHandler):
 	
-	def __init__(self, request, client_address, server):
-		SocketServer.BaseRequestHandler.__init__(self, request, client_address, server)
-		return
-
-	def setup(self):
-		return SocketServer.BaseRequestHandler.setup(self)
-
 	def handle(self):
-		# Echo the back to the client
 		data = self.request.recv(1024)
 		# Call something
 		print "GOT STUFF: %s" % data
+		self.request.close()
 		return
-
-	def finish(self):
-		return SocketServer.BaseRequestHandler.finish(self)
-
-# Server
-import SocketServer
-class SecPiTCPServer(SocketServer.TCPServer):
-	
-	def __init__(self, server_address, handler_class=SecPiTCPHandler):
-		SocketServer.TCPServer.__init__(self, server_address, handler_class)
-		return
-
-	def server_activate(self):
-		SocketServer.TCPServer.server_activate(self)
-		return
-
-	def serve_forever(self):
-		while True:
-			self.handle_request()
-		return
-
-	def handle_request(self):
-		return SocketServer.TCPServer.handle_request(self)
-
-	def verify_request(self, request, client_address):
-		return SocketServer.TCPServer.verify_request(self, request, client_address)
-
-	def process_request(self, request, client_address):
-		return SocketServer.TCPServer.process_request(self, request, client_address)
-
-	def server_close(self):
-		return SocketServer.TCPServer.server_close(self)
-
-	def finish_request(self, request, client_address):
-		return SocketServer.TCPServer.finish_request(self, request, client_address)
-
-	def close_request(self, request_address):
-		return SocketServer.TCPServer.close_request(self, request_address)
-
