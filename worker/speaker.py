01943bdea2087afb8d38f8ec4f56cfced4da9975
Martin Liebl <is141513@fhstp.ac.at>
2016-01-02 12:50:39 +0100

    speaker: added logging and error management for speaker action

--- worker/speaker.py
+++ worker/speaker.py
@@ -1,18 +1,33 @@
 import pygame
-from tools.action import Action
+import logging
 
+from tools.action import Action
 
 class Speaker(Action):
 
 	def __init__(self, id, params):
 		super(Speaker, self).__init__(id, params)
-		self.path_to_audio = params["path_to_audio"]
-		self.repetitions = params["repetitions"] #TODO: change
+		try:
+			self.path_to_audio = params["path_to_audio"]
+			self.repetitions = int(params["repetitions"]) #TODO: change| why did i write this??
+		except ValueError, e: # if repetitions can't be parsed as int
+			logging.error("Speaker: Wasn't able to initialize the device, please check your configuration: %s" % e)
+			return
+		except KeyError, k: # if config parameters are missing in file
+			logging.error("Speaker: Wasn't able to initialize the device, it seems there is a config parameter missing: %s" % k)
+			return
+
+		logging.debug("Speaker: Audio device initialized")
 
 	def play_audio(self):
-		print "playing audio"
+		logging.debug("Speaker: Trying to play audio")
 		pygame.mixer.init()
-		pygame.mixer.music.load(self.path_to_audio)
+		try:
+			pygame.mixer.music.load(self.path_to_audio)
+		except Exception, e: # audio file doesn't exist or is not playable
+			logging.error("Speaker: Wasn't able to load audio file: %s" % e)
+			pygame.mixer.quit()
+			return
 		pygame.mixer.music.set_volume(1)
 
 		for i in range(0, self.repetitions):
@@ -21,11 +36,10 @@ class Speaker(Action):
 			while pygame.mixer.music.get_busy():
 				continue
 		pygame.mixer.quit()
-		print "finished playing audio"
+		logging.debug("Speaker: Finished playing audio")
 
 	def execute(self):
 		self.play_audio()
 		
 	def cleanup(self):
-		# do some cleanup
-		
+		logging.debug("Speaker: No cleanup necessary at the moment")		
