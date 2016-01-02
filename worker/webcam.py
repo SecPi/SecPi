e7a29c63e9646aafc0d4d114ddf0412f0ae7d868
Martin Liebl <is141513@fhstp.ac.at>
2016-01-02 12:58:16 +0100

    webcam: added exception if a configuration parameter is missing, changed some logging too

--- worker/webcam.py
+++ worker/webcam.py
@@ -10,18 +10,24 @@ class Webcam(Action):
 	def __init__(self, id, params):
 		super(Webcam, self).__init__(id, params)	
 
-		self.path = params["path"]
 		try:
+			self.path = params["path"]
 			self.resolution = (int(params["resolution_x"]), int(params["resolution_y"]))
+			self.data_path = params["data_path"]
 		except ValueError, e: # if resolution can't be parsed as int
 			logging.error("Webcam: Wasn't able to initialize the device, please check your configuration: %s" % e)
 			return
-		self.data_path = params["data_path"]
+		except KeyError, k: # if config parameters are missing in file
+			logging.error("Webcam: Wasn't able to initialize the device, it seems there is a config parameter missing: %s" % k)
+			return
+
 		pygame.camera.init()
 		self.cam = pygame.camera.Camera(self.path, self.resolution)
 		logging.debug("Webcam: Video device initialized: %s" % self.path)
 
+	# take a series of pictures within a given interval
 	def take_adv_picture(self, num_of_pic, seconds_between):
+		logging.debug("Webcam: Trying to take pictures")
 		try:
 			self.cam.start()
 		except SystemError, e: # device path wrong
@@ -40,6 +46,8 @@ class Webcam(Action):
 			logging.error("Webcam: Wasn't able to take pictures: %s" % e)
 
 		self.cam.stop()
+		logging.debug("Webcam: Finished taking pictures")
+
 	
 	def execute(self):
 		self.take_adv_picture(int(self.params["count"]), int(self.params["interval"]))
