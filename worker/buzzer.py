2e5cff4f156fcc2f62435f64850f620d02997898
Martin Liebl <is141513@fhstp.ac.at>
2016-01-02 14:34:33 +0100

    buzzer: implemented logging and error management for buzzer action

--- worker/buzzer.py
+++ worker/buzzer.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import logging
 import time
 
 from tools.action import Action
@@ -7,12 +8,29 @@ class Buzzer(Action):
 
 	def __init__(self, id, params):
 		super(Buzzer, self).__init__(id, params)
-		self.duration = int(params["duration"])
-		self.gpio_pin = int(params["gpio_pin"])
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(self.gpio_pin, GPIO.OUT)
+		try:
+			self.duration = int(params["duration"])
+			self.gpio_pin = int(params["gpio_pin"])
+		except KeyError, k: # if config parameters are missing in file
+			logging.error("Buzzer: Wasn't able to initialize the device, it seems there is a config parameter missing: %s" % k)
+			self.corrupted = True
+			return
+		except ValueError, v: # if a parameter can't be parsed as int
+			logging.error("Buzzer: Wasn't able to initialize the device, please check your configuration: %s" % v)
+			self.corrupted = True
+			return
+
+		try:
+			GPIO.setmode(GPIO.BCM)
+			GPIO.setup(self.gpio_pin, GPIO.OUT)
+		except ValueError, v: # GPIO pin number is not useable e.g. 999
+			logging.error("Buzzer: The given pin number is not in a valid range: %s" % v)
+			self.corrupted = True
+			return
+		logging.debug("Buzzer: Audio device initialized")
 
 	def buzz(self, duration):
+		logging.debug("Buzzer: Trying to make some noise")
 		start_time = time.time()
 		state = True
 		while (time.time() - start_time) < duration:
@@ -23,11 +41,18 @@ class Buzzer(Action):
 			else:
 				state = True
 				time.sleep(0.05)
-		# set output to low again, because cleanup doesn't work yet
+		
 		state = False
 		GPIO.output(self.gpio_pin, state)
-		print "Finished buzzing"
-	# TODO: implement cleanup for gpio
+		logging.debug("Buzzer: Finished making noise")
 
 	def execute(self):
-		self.buzz(self.duration)
+		if not self.corrupted:
+			self.buzz(self.duration)
+		else:
+			logging.error("Buzzer: Wasn't able to make noise because of an initialization error")
+
+
+	def cleanup(self):
+		if not self.corrupted:
+			GPIO.cleanup(self.gpio_pin)
