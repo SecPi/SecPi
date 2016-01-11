495eade4f060e95e4d74d67b1ed5a97798039935
Philip Woelfel <is141524@fhstp.ac.at>
2016-01-11 09:41:31 +0100

    create_htdigest can now update an existing file (fixes #38)

--- webinterface/create_htdigest.sh
+++ webinterface/create_htdigest.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-HASH=`echo -n $2:secpi:$3 | md5sum |awk '{print $1}'`
+HASH=`echo -n $2:secpi:$3 | md5 |awk '{print $1}'`
 LINE="$2:secpi:$HASH"
-echo $LINE > $1
+# file already exists, do some magic
+if [ -f $1 ]
+then
+	# check if name already in file
+	if grep $2:secpi $1 > /dev/null 2>&1
+	then
+		sed -i '' "s/$2:secpi:.*/$2:secpi:$HASH/" "$1"
+	else
+		# else append to file
+		echo $LINE >> $1
+	fi
+else
+	echo $LINE > $1
+fi
