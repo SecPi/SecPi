b6de34b3a4e262409e6adb8826c770123b2445ef
Philip Woelfel <is141524@fhstp.ac.at>
2016-02-08 22:33:21 +0100

    webinterface: added hidden and fixed type for edit form (fixes #60) Commit number 500 yay :)                                  .''.        .''.             *''*    :_\/_:     .       :_\/_:   .    .:.*_\/_*   : /\ :  .'.:.'.   .''.: /\ : _\(/_  ':'* /\ *  : '..'.  -=:o:=-  :_\/_:'.:::. /)\*''*  .|.* '.\'/.'_\(/_'.':'.'  : /\ : :::::  '*_\/_* | |  -= o =- /)\    '  *   '..'  ':::'   * /\ * |'|  .'/.\'.  '._____       *        __*..* |  |     :      |.   |' .---"|        _*   .-'   '-. |  |     .--'|  ||   | _|    |     .-'|  _.|  |    ||   '-__  |   |  |    ||      |     |' | |.    |    ||       | |   |  |    ||      |  ___|  '-'     '    ""       '-'   '-.'    '`      |____ jgs~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- webinterface/sites/params.py
+++ webinterface/sites/params.py
@@ -14,7 +14,7 @@ class ParamsPage(BaseWebPage):
 	def __init__(self):
 		super(ParamsPage, self).__init__(objects.Param)
 		self.fields['id'] = {'name':'ID', 'visible':['list']}
-		self.fields['object_id'] = {'name':'Object ID', 'visible':['list', 'add', 'update'], 'type':'number'}
+		self.fields['object_id'] = {'name':'Object ID', 'visible':['list', 'add', 'update'], 'type':'number', 'default':0}
 		self.fields['object_type'] = {'name':'Type', 'visible':['list', 'add', 'update']}
 		self.fields['key'] = {'name':'Key', 'visible':['list', 'add', 'update']}
 		self.fields['value'] = {'name':'Value', 'visible':['list', 'add', 'update']}
