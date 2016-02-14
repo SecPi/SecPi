fdaf3dc9fbe21c1578f60d20c2fa6dd3c4a5a720
Philip Woelfel <is141524@fhstp.ac.at>
2016-02-14 22:27:20 +0100

    webinterface: updated descriptions of paramters

--- webinterface/sites/actionparams.py
+++ webinterface/sites/actionparams.py
@@ -14,7 +14,7 @@ class ActionParamsPage(BaseWebPage):
 	def __init__(self):
 		super(ActionParamsPage, self).__init__(objects.Param)
 		self.fields['id'] = {'name':'ID', 'visible':['list']}
-		self.fields['object_id'] = {'name':'Object ID', 'visible':['list', 'add', 'update'], 'type':'number', 'default':0}
+		self.fields['object_id'] = {'name':'Action ID', 'visible':['list', 'add', 'update'], 'type':'number', 'default':0}
 		self.fields['object_type'] = {'name':'Type', 'visible':['add', 'update'], 'type':'hidden', 'default':'action'}
 		self.fields['key'] = {'name':'Key', 'visible':['list', 'add', 'update']}
 		self.fields['value'] = {'name':'Value', 'visible':['list', 'add', 'update']}
