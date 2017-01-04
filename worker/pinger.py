7b764d74758d01179dc8233e6f6550273457cca8
Martin Liebl <is141513@fhstp.ac.at>
2017-01-04 22:53:10 +0100

    Pinger: updated log messages, for now checking only for consecutive losses

--- worker/pinger.py
+++ worker/pinger.py
@@ -15,11 +15,11 @@ class Pinger(Sensor):
 			self.destination_ip = params["destination_ip"]
 			self.bouncetime = int(params["bounce_time"])
 		except KeyError as ke: # if config parameters are missing in file
-			logging.error("Pinger: Wasn't able to initialize, it seems there is a config parameter missing: %s" % ke)
+			self.post_err("Pinger: Wasn't able to initialize, it seems there is a config parameter missing: %s" % ke)
 			self.corrupted = True
 			return
 		except ValueError as ve: # if a parameter can't be parsed as int
-			logging.error("Pinger: Wasn't able to initialize, please check your configuration: %s" % ve)
+			self.post_err("Pinger: Wasn't able to initialize, please check your configuration: %s" % ve)
 			self.corrupted = True
 			return
 
@@ -28,17 +28,16 @@ class Pinger(Sensor):
 	def activate(self):
 		if not self.corrupted:
 			self.stop_thread = False
-			self.pinger_thread = threading.Thread(name="thread-pinger-%s" % self.destination_ip,
-												  target=self.check_up)
+			self.pinger_thread = threading.Thread(name="thread-pinger-%s" % self.destination_ip, target=self.check_up)
 			self.pinger_thread.start()
 		else:
-			logging.error("Pinger: Sensor couldn't be activated")
+			self.post_err("Pinger: Sensor couldn't be activated")
 
 	def deactivate(self):
 		if not self.corrupted:
 			self.stop_thread = True
 		else:
-			logging.error("Pinger: Sensor couldn't be deactivated")
+			self.post_err("Pinger: Sensor couldn't be deactivated")
 
 
 	def check_up(self):
@@ -48,14 +47,14 @@ class Pinger(Sensor):
 				return
 
 			reply = pyping.ping(self.destination_ip) # ret value is 0 when reachable
-			logging.info("ret value: %d" % reply.ret_code)
 			if reply.ret_code:
 				losses += 1
-				logging.info("Loss happened: %d/%d" % (losses, self.max_losses))
+				logging.info("Pinger: Loss happened, %d/%d" % (losses, self.max_losses))
+			else:
+				losses = 0
 
 			if losses >= self.max_losses:
-				logging.info("Alarm")
-				self.alarm("Too many pings lost")
+				self.alarm("%d consecutive pings were lost" % losses)
 				losses = 0
 				time.sleep(self.bouncetime)
 				continue
