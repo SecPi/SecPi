e57c0e9bae27c645a1ea615924a300626695a60e
Andreas Motl <andreas.motl@elmyra.de>
2022-08-15 23:36:11 +0200

    Worker: Connect to vanilla RabbitMQ at `localhost:5672` by default

--- worker/worker.py
+++ worker/worker.py
@@ -70,9 +70,9 @@ class Worker:
 		logging.debug("Initalizing network connection")
 		credentials = pika.PlainCredentials(config.get('rabbitmq')['user'], config.get('rabbitmq')['password'])
 		parameters = pika.ConnectionParameters(credentials=credentials,
-			host=config.get('rabbitmq')['master_ip'],
-			port=5671,
 			ssl=True,
+			host=config.get('rabbitmq', {}).get('master_ip', 'localhost'),
+			port=int(config.get('rabbitmq', {}).get('master_port', 5672)),
 			socket_timeout=10,
 			ssl_options = { 
 				"ca_certs":PROJECT_PATH+"/certs/"+config.get('rabbitmq')['cacert'],
