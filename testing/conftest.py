9b29efdcbdf37b2ce84f58d99728add5afb55e48
Andreas Motl <andreas.motl@cicerops.de>
2022-08-27 15:42:58 +0200

    Tests: Cleanly shut down worker after running the test case
    It is important to make code coverage recording across subprocesses
    work correctly.

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,3 +1,4 @@
+import subprocess
 import sys
 import time
 
@@ -11,6 +12,15 @@ from testing.util.process import capturing_run
 def worker_daemon(capsys):
     sys.argv = ["secpi-worker", "--app-config=testing/etc/config-worker.json"]
     proc = capturing_run(target=worker.worker.main, args=())
-    time.sleep(3.85)
+    time.sleep(0.85)
     yield proc
+
+    # 1. Send shutdown signal to make the service terminate itself.
+    command = """echo '{"action": "shutdown"}' | amqp-publish --routing-key=secpi-op-1"""
+    print(subprocess.check_output(command, shell=True), file=sys.stderr)
+    time.sleep(0.75)
+
+    # 2. Terminate service process.
+    # In this case, there will be no code coverage information. Because the process did
+    # not shut down cleanly, it failed to record it.
     proc.terminate()
