809e645fdacfb24b6bfff37a24b4530215624549
Andreas Motl <andreas.motl@cicerops.de>
2022-09-04 01:45:08 +0200

    Tests: Add full stack test
    This tests the whole scenario of:
    
    Worker -> TCP sensor -> Alarm -> Manager
      -> Action -> Worker 
      -> Notifier

--- testing/test_system.py
+++ testing/test_system.py
@@ -1,6 +1,8 @@
 import json
+import re
 import subprocess
 import time
+from email.message import Message
 
 import requests
 
@@ -11,7 +13,7 @@ WORKER_ENTITY = {"id": 1, "name": "worker-testing", "address": "localhost"}
 NOTIFIER_ENTITY = {"id": 1, "name": "mailer-testing", "module": "mailer", "cl": "Mailer", "active_state": True}
 NOTIFIER_PARAMS = [
     {"object_id": 1, "object_type": "notifier", "key": "smtp_address", "value": "localhost"},
-    {"object_id": 1, "object_type": "notifier", "key": "smtp_port", "value": "12525"},
+    {"object_id": 1, "object_type": "notifier", "key": "smtp_port", "value": "8025"},
     {"object_id": 1, "object_type": "notifier", "key": "smtp_user", "value": ""},
     {"object_id": 1, "object_type": "notifier", "key": "smtp_pass", "value": ""},
     {"object_id": 1, "object_type": "notifier", "key": "smtp_security", "value": "NOAUTH_NOSSL"},
@@ -30,6 +32,9 @@ ACTION_WORKER_ENTITY = {"action_id": 1, "worker_id": 1}
 
 
 def create_and_activate_setup(with_notifier=False, with_worker=False, with_action=False):
+    """
+    Populate the database, using the HTTP API.
+    """
 
     # Create a setup.
     requests.post(
@@ -67,6 +72,7 @@ def test_manager_process_alarm(webinterface_service, manager_service):
     Verify that the log output matches the expectations.
     """
 
+    # Populate database, using the HTTP API.
     create_and_activate_setup(with_notifier=True)
 
     # Emulate an alarm signal using AMQP.
@@ -121,3 +127,97 @@ def test_manager_process_alarm(webinterface_service, manager_service):
         or "ConnectionRefusedError: [Errno 111] Connection refused" in app_log
         or "OSError: [Errno 99] Cannot assign requested address" in app_log
     )
+
+
+def test_full_stack(manager_service, worker_service, webinterface_service, smtpd):
+    """
+    Start the full stack and invoke an alarm signal.
+    Verify that the sensor triggered and all notifiers and actions have been invoked correctly.
+    """
+
+    # Populate database, using the HTTP API.
+    create_and_activate_setup(with_worker=True, with_notifier=True, with_action=True)
+
+    # Submit a sensor signal using TCP.
+    command = "echo hello | socat - tcp:localhost:1234"
+    subprocess.check_output(command, shell=True)
+
+    # Give system some time for processing.
+    time.sleep(0.50)
+
+    # Read application log.
+    manager_log = manager_service.read_log()
+    worker_log = worker_service.read_log()
+
+    # Verify everything is in place.
+    assert "Loading class successful: secpi.action.test.TestAction" in worker_log
+    assert "Test Action initialized" in worker_log
+
+    assert "Registering action: 1" in worker_log
+    assert "Registered action: {'id': 1" in worker_log
+    # assert "Registering action: 2" in app_log
+    # assert "Registered action: {'id': 2" in app_log
+    assert "Setup of sensors and actions completed" in worker_log
+
+    assert "Alarm from sensor id=1, worker worker-testing: Got TCP connection, raising alarm" in manager_log
+    assert "Created directory for alarm:" in manager_log
+    assert "Executing actions" in manager_log
+
+    assert "Executing Test Action" in worker_log
+
+    assert "Test Action message: foobar" in worker_log
+    assert "Creating file artefacts at /tmp/secpi-testing" in worker_log
+    assert "Created ZIP file" in worker_log
+    assert "Publishing message: {'exchange': 'secpi', 'routing_key': 'secpi-action-response'" in worker_log
+    assert "Sent data to manager" in worker_log
+    assert "Cleaned up files" in worker_log
+
+    assert "Got log message from Worker 1: Executing Test Action" in manager_log
+    assert "Got log message from Worker 1: Test Action message: foobar" in manager_log
+    assert "Received response from action invocation" in manager_log
+    assert re.match(r".*Writing data to current alarm dir:.*\.zip", manager_log, re.DOTALL | re.MULTILINE)
+
+    assert "Received all data from workers, cancelling the timeout" in manager_log
+    assert "Notifying via SMTP email" in manager_log
+
+    assert "Mailer: Attached file 'index.txt' to message" in manager_log
+    assert "Mailer: Attached file 'index.json' to message" in manager_log
+    assert "Mailer: Attached file 'index.xml' to message" in manager_log
+
+    assert "Mailer: Trying to send mail without authentication" in manager_log
+    assert "Mailer: Establishing connection to SMTP server" in manager_log
+
+    assert len(smtpd.messages) == 1
+
+    message: Message = smtpd.messages[0]
+
+    assert message.is_multipart()
+    assert message.get("From") == "secpi@example.org"
+    assert message.get("To") == "user@example.org"
+    assert message.get("Subject") == "SecPi Alarm"
+
+    parts = []
+    for message in message.get_payload():
+        part = dict(
+            body=message.get_payload().strip(),
+            type=message.get("Content-Type"),
+            disposition=message.get("Content-Disposition"),
+        )
+        parts.append(part)
+    assert (
+        dict(body="WW91ciBTZWNQaSByYWlzZWQgYW4gYWxhcm0=", type='text/plain; charset="utf-8"', disposition=None) in parts
+    )
+    assert (
+        dict(
+            body="Received alarm on sensor id=1 from worker worker-testing: Got TCP connection, raising alarm",
+            type='text/plain; charset="us-ascii"',
+            disposition=None,
+        )
+        in parts
+    )
+
+    attachments = sorted([part["disposition"] for part in parts if part["disposition"]])
+    assert len(attachments) == 5
+    assert 'attachment; filename="index.json"' in attachments
+    assert 'attachment; filename="index.txt"' in attachments
+    assert 'attachment; filename="index.xml"' in attachments
