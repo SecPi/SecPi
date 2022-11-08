61b4dee7e29cf1acf49c96d1f55b1925992f6a9e
Andreas Motl <andreas.motl@cicerops.de>
2022-11-08 23:02:11 +0100

    Database: Work around data model violating foreign key constraints
    The current data model is not 100% compatible with SQLAlchemy
    SQLite vs. MySQL, because it violates foreign key constraints on the
    relationship between {`Action`, `Notifier`, `Sensor`} and `Param`
    entities.
    
    In order to work around it, disable foreign key checking when using
    MySQL/MariaDB.
    
    See also: https://github.com/isarengineering/SecPi/issues/37

--- secpi/util/database.py
+++ secpi/util/database.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import typing as t
 
 from sqlalchemy import create_engine
@@ -22,10 +23,33 @@ class DatabaseAdapter:
         self.session: t.Optional[Session] = None
 
     def connect(self):
+
+        # Feed a newline to the logger when running the test suite.
+        if "PYTEST_CURRENT_TEST" in os.environ:
+            logger.info("")
+
         logger.info(f"Connecting to database {self.uri}")
 
         # `echo = True` activates debug logging.
         self.engine = create_engine(self.uri, echo=False)
+
+        # FIXME: The current data model is not 100% compatible with SQLAlchemy SQLite+MySQL,
+        #        because it violates foreign key constraints on the relationship between
+        #        {`Action`, `Notifier`, `Sensor`} and `Param` entities.
+        #        See also: https://github.com/isarengineering/SecPi/issues/37
+        #
+        # ERROR:
+        #
+        #   sqlalchemy.exc.IntegrityError: (pymysql.err.IntegrityError)
+        #   Cannot add or update a child row: a foreign key constraint fails (1452)
+        #   `params` CONSTRAINT `lalala` FOREIGN KEY (`object_id`) REFERENCES `{actions,sensors,notifiers}` (`id`))'
+        #
+        # SOLUTION / WORKAROUND:
+        #
+        #   Disable foreign key checks on MySQL/MariaDB.
+
+        if self.uri.startswith("mysql"):
+            self.engine.execute("SET FOREIGN_KEY_CHECKS = 0;")
 
         create_session = sessionmaker(bind=self.engine)
         self.session = create_session()
