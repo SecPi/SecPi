be2fb329e6f11633e39176f4e8c29fed0ea15d5b
Andreas Motl <andreas.motl@cicerops.de>
2022-11-10 03:42:41 +0100

    Tests: Adjust `test_system` according to recent database schema changes

--- testing/test_system.py
+++ testing/test_system.py
@@ -12,21 +12,21 @@ WORKER_ENTITY = {"id": 1, "name": "worker-testing", "address": "localhost"}
 
 NOTIFIER_ENTITY = {"id": 1, "name": "mailer-testing", "module": "mailer", "cl": "Mailer", "active_state": True}
 NOTIFIER_PARAMS = [
-    {"object_id": 1, "object_type": "notifier", "key": "smtp_address", "value": "localhost"},
-    {"object_id": 1, "object_type": "notifier", "key": "smtp_port", "value": "8025"},
-    {"object_id": 1, "object_type": "notifier", "key": "smtp_user", "value": ""},
-    {"object_id": 1, "object_type": "notifier", "key": "smtp_pass", "value": ""},
-    {"object_id": 1, "object_type": "notifier", "key": "smtp_security", "value": "NOAUTH_NOSSL"},
-    {"object_id": 1, "object_type": "notifier", "key": "sender", "value": "secpi@example.org"},
-    {"object_id": 1, "object_type": "notifier", "key": "recipient", "value": "user@example.org"},
-    {"object_id": 1, "object_type": "notifier", "key": "subject", "value": "SecPi Alarm"},
-    {"object_id": 1, "object_type": "notifier", "key": "text", "value": "Your SecPi raised an alarm"},
-    {"object_id": 1, "object_type": "notifier", "key": "unzip_attachments", "value": "1"},
+    {"notifier_id": 1, "object_type": "notifier", "key": "smtp_address", "value": "localhost"},
+    {"notifier_id": 1, "object_type": "notifier", "key": "smtp_port", "value": "8025"},
+    {"notifier_id": 1, "object_type": "notifier", "key": "smtp_user", "value": ""},
+    {"notifier_id": 1, "object_type": "notifier", "key": "smtp_pass", "value": ""},
+    {"notifier_id": 1, "object_type": "notifier", "key": "smtp_security", "value": "NOAUTH_NOSSL"},
+    {"notifier_id": 1, "object_type": "notifier", "key": "sender", "value": "secpi@example.org"},
+    {"notifier_id": 1, "object_type": "notifier", "key": "recipient", "value": "user@example.org"},
+    {"notifier_id": 1, "object_type": "notifier", "key": "subject", "value": "SecPi Alarm"},
+    {"notifier_id": 1, "object_type": "notifier", "key": "text", "value": "Your SecPi raised an alarm"},
+    {"notifier_id": 1, "object_type": "notifier", "key": "unzip_attachments", "value": "1"},
 ]
 
 ACTION_ENTITY = {"id": 1, "name": "action-testing", "module": "test", "cl": "TestAction", "active_state": True}
 ACTION_PARAMS = [
-    {"object_id": 1, "object_type": "action", "key": "msg", "value": "foobar"},
+    {"action_id": 1, "object_type": "action", "key": "msg", "value": "foobar"},
 ]
 ACTION_WORKER_ENTITY = {"action_id": 1, "worker_id": 1}
 
